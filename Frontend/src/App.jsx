69055982abd39e43550efae4a61a634c3e8b0abf
Himanshu Verma <himanshuverma4765@gmail.com>
2026-01-01 16:40:20 +0530

    Routing Done!

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,30 @@
-import HomePage from "./landing_page/home/HomePage"
+import { Routes, Route } from "react-router-dom";
+import Home from "./landing_page/home/HomePage";
+import About from "./landing_page/about/AboutPage";
+import Pricing from "./landing_page/pricing/PricingPage";
+import Signup from "./landing_page/signup/Signup";
+import Products from "./landing_page/products/ProductsPage";
+import Support from "./landing_page/support/SupportPage";
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
+import NotFound from "./landing_page/NotFound";
 
 function App() {
-
   return (
     <>
-<HomePage/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
